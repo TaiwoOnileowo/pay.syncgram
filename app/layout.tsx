a209a7ffb95a07f74cf866a9f9d2176b76646ee9
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2025-05-08 10:04:34 +0100

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,24 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "SyncGram Payment Gateway",
   description: "Secure and fast payment processing for Telegram groups.",
+  openGraph: {
+    title:
+      "SyncGram Payment Gateway - Manage Telegram Payments and Subscriptions",
+    description:
+      "SyncGram helps Telegram group owners manage payments and subscriptions in their telegram groups seamlessly.",
+    images: [
+      {
+        url: "https://www.sync-gram.com/logo1.png",
+        width: 1200,
+        height: 630,
+        alt: "SyncGram",
+      },
+    ],
+    url: "https://www.pay.sync-gram.com",
+    type: "website",
+    locale: "en_US",
+    siteName: "SyncGram",
+  },
 };
 
 export default function RootLayout({
